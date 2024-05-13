e21e78116cf30499c33e96ced1ee245dca04f4ba
Hungvy24 <hungvydz2004@gmail.com>
2024-05-13 10:15:36 +0700

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { first, last } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,33 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'projectName';
+  // title = 'Thong tin sinh vien';
+  // student ={
+  //   ten: 'Nguyen Hung Vy',
+  //   ngaysinh: '10/03/2004',
+  //   diachi: 'Ha Noi',
+  //   gioitinh: 'Nam',
+  //   diemTB: 9.5
+  // }
+  // // invevtors: Inventor[] = [
+  // //   {
+  // //     id: 1, first: 'Vy', last: 'Nguyen', year: 2004, passed: 2010
+  // //   },
+  // //   {
+  // //     id: 2, first: 'Huong', last: 'Nguyen', year: 2004, passed: 2011
+  // //   },
+  // //   {
+  // //     id: 3, first: 'Huy', last: 'Nguyen', year: 2004, passed: 2012
+  // //   },
+  // //   {
+  // //     id: 4, first: 'Cong', last: 'Nguyen', year: 2004, passed: 2013
+  // //   },
+  // //   {
+  // //     id: 5, first: 'Bao', last: 'Nguyen', year: 2004, passed: 2014
+  // //   },
+  // //   {
+  // //     id: 6, first: 'Dung', last: 'Nguyen', year: 2004, passed: 2015
+  // //   }
+  // // ]
 }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './pages/admin/products/list/list.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: 'products/list',
+        component: ProductListComponent
+      }
+    ]
+  }
+];
